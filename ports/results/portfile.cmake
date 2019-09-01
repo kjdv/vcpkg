37e35a6d7a3abb6f3368ee943fb3a3758a954557
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-09-01 11:27:49 +0100

    results only from head

--- ports/results/portfile.cmake
+++ ports/results/portfile.cmake
@@ -2,8 +2,6 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REF 1.0
-    SHA512 1e57024d12f42b4caa4ef6b4d3fb9879f36c589c5d8fac295010e393055c1dd7cf4b68eafc97bd36203f389f19ac2bffda2a91cc9583ad7f0aaa952a46be74f5
     REPO kjdv/results
     HEAD_REF master
 )
