882af0fb8d1d8eb999b6da3f403ac03852484e17
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-09-04 08:19:02 +0100

    rebuild world script

---
+++ rebuild_world.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+
+vcpkg = os.path.join(os.path.dirname(__file__), 'vcpkg')
+
+# in dependency order
+packages = [ 
+    'results',
+    'kb64',
+    'composite',
+    'krc',
+
+    'kyaml',
+    'kthread',
+]
+
+def banner(msg):
+    print('*' * len(msg))
+    print(msg)
+    print('*' * len(msg))
+
+for package in reversed(packages):
+    banner(f'removing {package}')
+    subprocess.check_call([vcpkg, 'remove', package])
+
+for package in packages:
+    banner(f'installing {package}')
+    subprocess.check_call([vcpkg, 'install', '--head', package])
+
+banner('done')
